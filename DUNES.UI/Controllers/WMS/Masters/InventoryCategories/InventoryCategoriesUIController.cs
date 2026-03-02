f5918571ab161c107d842fb7b37819d73c25b6f8
Herledy Lopez <herledylopez@gmail.com>
2026-03-02 16:15:49 -0500

    202603021615

--- DUNES.UI/Controllers/WMS/Masters/InventoryCategories/InventoryCategoriesUIController.cs
+++ DUNES.UI/Controllers/WMS/Masters/InventoryCategories/InventoryCategoriesUIController.cs
@@ -193,12 +193,55 @@ namespace DUNES.UI.Controllers.WMS.Masters.InventoryCategories
             }, ct);
         }
 
+
+        /// <summary>
+        /// Edit page.
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id, CancellationToken ct)
+        {
+            if (CurrentToken is null)
+                return RedirectToLogin();
+
+            await SetMenuBreadcrumbAsync(
+                MENU_CODE_CRUD,
+                _menuClientService,
+                ct,
+                CurrentToken,
+                new BreadcrumbItem { Text = "Delete Catetory", Url = null });
+
+            return await HandleAsync(async ct =>
+            {
+                var res = await _service.GetByIdAsync(id, CurrentToken!, ct);
+
+                if (!res.Success || res.Data is null)
+                {
+                    MessageHelper.SetMessage(this, "danger", res.Message ?? "Category not found.", MessageDisplay.Inline);
+                    return RedirectToAction(nameof(Index));
+                }
+
+                // ReadDTO -> UpdateDTO
+                var model = new WMSInventorycategoriesReadDTO
+                {
+                    Id = res.Data.Id,
+                    Name = res.Data.Name,
+                    Observations = res.Data.Observations,
+                    CycleCountDays = res.Data.CycleCountDays,
+                    ErrorTolerance = res.Data.ErrorTolerance,
+                    Active = res.Data.Active
+                };
+
+                return View(model);
+            }, ct);
+        }
+
+
         /// <summary>
         /// Hard delete (Option B): only if not used.
         /// </summary>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Delete(int id, CancellationToken ct)
+        public async Task<IActionResult> Delete(int id, WMSInventorycategoriesUpdateDTO dto, CancellationToken ct)
         {
             if (CurrentToken is null)
                 return RedirectToLogin();
